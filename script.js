e929b3ea168553a3e6006bcabc0f9de90f732efd
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-22 01:57:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const makeChange = (c) => {
   // your name here
+	const q=Math.floor(c/25);
+	const remainingCentsAfterQuarters=c%25;
+	const d=Math.floor(remainingCentsQuarters/10);
+	const remainingCentsAfterDimes=remainingCentsAfterQuarters%10;
+	const n=Math.floor(remainingCentsAfterDimes/5);
+	const remainingCentsAfterNickels=remainingCentsAfterDimes%5;
+	const p=remainingCentsAfterNickels;
+	return{"q":q,"d":}d,"n":n,"p":p};
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
